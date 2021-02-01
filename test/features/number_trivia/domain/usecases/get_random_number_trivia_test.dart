0a70c1f6e83e37c50e84dd9796abf3db4b82cd35
lightScout <juanmullercs@hotmail.com>
2021-02-01 13:15:52 +0000

    .Domain Layer Refactoring

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,7 @@
+import 'package:clean_architecture_tdd/core/usecases/usecase.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:clean_architecture_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:dartz/dartz.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,27 +10,26 @@ class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
 void main() {
-  GetConcreteNumberTrivia usecase;
+  GetRandomNumberTrivia usecase;
   MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
-  final tNumber = 1;
   final tNumberTrivia = NumberTrivia(text: 'test', number: 1);
 
   test(
-    'should get trivia fro the number from the repository',
+    'should get trivia from the repository',
     () async {
       // arrange
-      when(mockNumberTriviaRepository.getConcreteNumbertrivia(any))
+      when(mockNumberTriviaRepository.getRandomNumbertrivia())
           .thenAnswer((_) async => Right(tNumberTrivia));
       // act
-      final result = await usecase(Params(number: tNumber));
+      final result = await usecase(NoParams());
       //assert
       expect(result, Right(tNumberTrivia));
-      verify(mockNumberTriviaRepository.getConcreteNumbertrivia(tNumber));
+      verify(mockNumberTriviaRepository.getRandomNumbertrivia());
       verifyNoMoreInteractions(mockNumberTriviaRepository);
     },
   );
